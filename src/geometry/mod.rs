cdb02f8dbd736d9000cf4c4b30d985b002116887
ojferro <ojferro@gmail.com>
2021-03-01 00:41:20 -0500

    Add material

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -2,7 +2,10 @@ pub use self::geometry::HitRecord;
 pub use self::geometry::Hittable;
 pub use self::geometry::Sphere;
 pub use self::geometry::HittableList;
+
 pub use self::geometry::Material;
+pub use self::geometry::Metal;
+// pub use self::geometry::Diffuse;
 
 mod geometry{
     use crate::vector::vec3;
@@ -16,7 +19,6 @@ mod geometry{
         pub normal: vec3,
         pub t: f64,
         pub front_face: bool,
-        pub material_ptr: Box<dyn Material>
     }
 
     impl HitRecord{
@@ -28,12 +30,12 @@ mod geometry{
 
     impl Default for HitRecord{
         fn default() -> Self {HitRecord{p: point3::new(0.0,0.0,0.0), normal: vec3::new(0.0,0.0,0.0),
-                              t: 0.0, front_face: true, ..Default::default()}}
+                              t: 0.0, front_face: true}}
     }
 
     ///////////////////////// Parent trait for all hittable geometry /////////////////////////
     pub trait Hittable {
-        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool;
+        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> Option<Ray>;
     }
 
     /////////////////////////// Sphere /////////////////////////
@@ -50,7 +52,7 @@ mod geometry{
     }
 
     impl Hittable for Sphere{
-        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool{
+        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> Option<Ray>{
             let oc: vec3 = ray.origin - self.center;
 
             let a = ray.dir.length_squared();
@@ -58,7 +60,7 @@ mod geometry{
             let c = oc.length_squared() - self.radius*self.radius;
 
             let discriminant = half_b*half_b-a*c;
-            if discriminant<0.0{ return false; }
+            if discriminant<0.0{ return None; }
             
             let d_sqrt = discriminant.sqrt();
 
@@ -67,7 +69,7 @@ mod geometry{
             if root<t_min || t_max<root {
                 root = (-half_b+d_sqrt)/a;
                 if root<t_min||t_max<root{
-                    return false;
+                    return None;
                 }
             }
 
@@ -78,7 +80,8 @@ mod geometry{
             let outward_normal = (hit_record.p - self.center)/self.radius;
             hit_record.set_face_normal(ray, &outward_normal);
 
-            return true;
+            let target: point3 = hit_record.p + hit_record.normal + vec3::random_unit_vector();
+            return Some(Ray::new(ray.at(root), target-hit_record.p).to_owned());
             
         }
     }
@@ -99,39 +102,51 @@ mod geometry{
     }
 
     impl Hittable for HittableList{
-        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool{
+        fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> Option<Ray>{
             let mut temp_hr = HitRecord{..Default::default()};
-            let mut hit_something = false;
             let mut closest_so_far = t_max;
+            let mut current_ray = None;
 
             for object in self.list.iter(){
-                if object.hit(ray, t_min, closest_so_far, &mut temp_hr){
-                    hit_something = true;
+                if let Some(r) = object.hit(ray, t_min, closest_so_far, &mut temp_hr){
                     closest_so_far = temp_hr.t;
 
                     hit_record.p = temp_hr.p;
                     hit_record.normal = temp_hr.normal;
                     hit_record.t = temp_hr.t;
                     hit_record.front_face = temp_hr.front_face;
+
+                    current_ray = Some(r);
                 }
             }
-            hit_something
+            current_ray
         }
     }
 
     // Material Class
     pub trait Material{
-        fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: colour, scattered: &mut Ray) -> &'static bool;
+        fn scatter(&self, r_in: &Ray, hit_record: &HitRecord, attenuation: colour, scattered: &mut Ray) -> &'static bool;
     }
-    pub struct Metal{
-        pub material: Box<dyn Material>,
-    }
+
+    pub struct Metal{}
+
     impl Material for Metal{
-        fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: colour, scattered: &mut Ray) ->  &'static bool{
+        fn scatter(&self, r_in: &Ray, hit_record: &HitRecord, attenuation: colour, scattered: &mut Ray) ->  &'static bool{
             &true
         }
     }
-    pub struct Diffuse{
-        pub material: Box<dyn Material>,
+
+    pub struct Lambertian{
+        pub albedo: colour
     }
+
+    // impl Material for Lambertian{
+    //     fn scatter(&self, r_in: &Ray, hit_record: &HitRecord, attenuation: colour, scattered: &mut Ray) ->  &'static bool{
+
+    //         let scatter_dir = hit_record.normal + vec3::random_unit_vector();
+    //         Ray::new(hit_record.p, scatter_dir);
+            
+    //     }
+    // }
+
 }

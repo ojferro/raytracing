074ede938918d2fb7ee694b84901bc30f0ae8acb
ojferro <ojferro@gmail.com>
2021-03-16 22:59:31 -0400

    Show image rendering line by line on window

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Built from scratch in Rust.
 
 ![alt text](https://github.com/ojferro/raytracing/blob/master/image.png?raw=true)
+
+## Instructions
+```
+sudo apt install libxkbcommon-dev libwayland-cursor0 libwayland-dev
+cargo run
+```
--- src/raytracer.rs
+++ src/raytracer.rs
@@ -1,3 +1,6 @@
+extern crate minifb;
+use minifb::{Key, Window, WindowOptions};
+
 use rand::Rng;
 
 use vector::vec3;
@@ -18,19 +21,12 @@ use geometry::Material;
 use geometry::*;
 mod geometry;
 
-use image::PPM;
-use image::RGB;
-mod image;
-
-
 use vec3 as colour;
 use vec3 as point3;
 
-// const ANTIALISAING: bool = true;
-const USE_BUFFER: bool = true; // Use buffer instead of outputting directly to file
 
 ////////////////////////// UTILITY FUNCTIONS /////////////////////////
-
+const USE_BUFFER: bool = true;
 /// COLOUR
 
 fn rand_f()->f64{
@@ -43,7 +39,12 @@ fn clamp(x: f64, min: f64, max: f64) -> f64{
     return x;
 }
 
-fn write_colour(mut colour: colour, samples_per_px: u32, buffer: &mut PPM, i: u32, j: u32){
+fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
+    let (r, g, b) = (r as u32, g as u32, b as u32);
+    (r << 16) | (g << 8) | b
+}
+
+fn write_colour(mut colour: colour, samples_per_px: u32, buffer: &mut Vec<u32>, i: usize, row: usize, image_width: usize, image_height: usize){
     let scale = 1.0/samples_per_px as f64;
     colour = colour*scale;
     
@@ -51,11 +52,15 @@ fn write_colour(mut colour: colour, samples_per_px: u32, buffer: &mut PPM, i: u3
     let ig = (256.0*clamp(colour.y, 0.0, 0.999)) as u8;
     let ib = (256.0*clamp(colour.z, 0.0, 0.999)) as u8;
 
-    if !USE_BUFFER{
-        // TODO: only about 4% faster to use buffer. Consider removing for simplicity.
-        print!("{} {} {}\n", ir, ig, ib);
-    }else{
-        buffer.set_pixel(i, j, RGB{r: ir, g: ig, b: ib});
+    buffer[row*image_width + i] = from_u8_rgb(ir, ig, ib);
+}
+
+fn write_to_window(window: &mut minifb::Window, buffer: &mut Vec<u32>, width: usize, height: usize){
+    if window.is_open() {
+
+        window
+            .update_with_buffer(&buffer, width, height)
+            .unwrap();
     }
 }
 
@@ -67,7 +72,8 @@ fn ray_colour(&ray: &Ray, scene: &dyn Hittable, ray_bounces: usize, gamma_correc
     let mut hr = geometry::HitRecord::default();
 
     let mut attenuation = colour::new(0.0,0.0,0.0);
-    if let Some(r) = scene.hit(&ray, &mut attenuation, 0.001, f64::INFINITY, &mut hr) { //hit anything in scene
+    let max_ray_len = f64::INFINITY;
+    if let Some(r) = scene.hit(&ray, &mut attenuation, 0.001, max_ray_len, &mut hr) { //hit anything in scene
         // Compute Lambertian reflection
         // TODO: Use r instead of recalculating it
         // let target: point3 = hr.p + hr.normal + vec3::random_unit_vector();
@@ -91,30 +97,39 @@ fn ray_colour(&ray: &Ray, scene: &dyn Hittable, ray_bounces: usize, gamma_correc
 fn main(){
     // IMAGE
     let aspect_ratio = 16.0/9.0 as f64;
-    let image_width: u32 = 1080;
-    let image_height = (image_width as f64/aspect_ratio) as u32;
+    let image_width: usize = 500;
+    let image_height = (image_width as f64/aspect_ratio) as usize;
 
-    let mut img_buffer = PPM::new(image_height.clone(), image_width.clone());
+    /////////// SET UP DISPAY /////////////
+    let mut img_buffer: Vec<u32> = vec![0; image_width * image_height];
+    let mut window = Window::new("Test - ESC to exit", image_width as usize, image_height as usize, WindowOptions::default())
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    // window.limit_update_rate(Some(std::time::Duration::from_micros(16666))); //1/60=16666mcs
+    ///////////////////////////////////////
     
     eprintln!("W: {}, H: {}", image_width, image_height);
 
     // Camera
     let origin = point3::new(0.0, 0.0, 0.0);
-    let samples_per_px = 200;
+    let samples_per_px = 100;
 
-    let cam_origin = point3::new(1.0,1.50,3.0);
-    let look_at = vec3::new(0.25,0.60,0.0);
+    let cam_origin = point3::new(1.0,1.30,3.0);
+    let look_at = vec3::new(0.25,0.60,-0.50);
     let mut cam = Camera::new(
         27.0,
         16.0/9.0 as f64,
-        0.0,
+        0.12,
         (cam_origin - look_at).length(),
         cam_origin,
         look_at,
         vec3::new(0.0,1.0,0.0),
         samples_per_px);
 
-    let max_ray_bounces = 50;
+    let max_ray_bounces = 10;
     let gamma_correction = true;
 
     // Scene
@@ -185,8 +200,9 @@ fn main(){
                     px_colour += ray_colour(&r, &scene, max_ray_bounces, gamma_correction);
                 }
                 let row;
-                if USE_BUFFER{ row = image_height-j; }else{ row = j;}
-                write_colour(px_colour, cam.samples_per_px, &mut img_buffer, i, row);
+                if USE_BUFFER{ row = image_height-1-j; }else{ row = j;}
+                // println!("i: {}, row: {}", i, j);
+                write_colour(px_colour, cam.samples_per_px, &mut img_buffer, i, row, image_width, image_height);
             }else{
                 let u = i as f64 / (image_width-1) as f64;
                 let v = j as f64 / (image_height-1) as f64;
@@ -195,10 +211,12 @@ fn main(){
 
                 let row;
                 if USE_BUFFER{ row = image_height-j; }else{ row = j;}
-                write_colour(px_colour, cam.samples_per_px, &mut img_buffer, i, row);
+                write_colour(px_colour, cam.samples_per_px, &mut img_buffer, i, row, image_width, image_height);
             }
         }
+        write_to_window(&mut window, &mut img_buffer, image_width as usize, image_height as usize);
     }
+    write_to_window(&mut window, &mut img_buffer, image_width as usize, image_height as usize);
     eprintln!("\nDone!");
-    img_buffer.write_file("image.ppm").expect("Error writing to file.");
+    while  window.is_open() && !window.is_key_down(Key::Escape) {}
 }

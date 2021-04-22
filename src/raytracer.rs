fbe1e79b795bb176bc1fb48093b2b0e3fe8c7ef7
ojferro <ojferro@gmail.com>
2021-04-21 20:34:15 -0400

    Move scene to the geometry module

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -147,43 +147,44 @@ fn main(){
     let gamma_correction = true;
 
     // Scene
-    let mut scene = HittableList::new();
+    // let mut scene = HittableList::new();
+    let mut scene = HittableList::get_scene();
 
-    // Yellow fuzzy metal sphere
-    let m1 = Box::new(geometry::Metal{albedo: colour::new(0.8, 0.6, 0.2), fuzz: 0.25});
-    let radius = 0.5;
-    scene.add(Box::new(Sphere::new(point3::new(0.80, radius, -1.0), radius, m1)));
+    // // Yellow fuzzy metal sphere
+    // let m1 = Box::new(geometry::Metal{albedo: colour::new(0.8, 0.6, 0.2), fuzz: 0.25});
+    // let radius = 0.5;
+    // scene.add(Box::new(Sphere::new(point3::new(0.80, radius, -1.0), radius, m1)));
 
-    // Red diffuse sphere
-    let m2: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.7, 0.3, 0.3)});
-    let radius = 0.25;
-    scene.add(Box::new(Sphere::new(point3::new(-0.10, radius, -0.10), radius, m2)));
+    // // Red diffuse sphere
+    // let m2: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.7, 0.3, 0.3)});
+    // let radius = 0.25;
+    // scene.add(Box::new(Sphere::new(point3::new(-0.10, radius, -0.10), radius, m2)));
 
-    // Shiny metal sphere
-    let m3: Box<dyn Material> = Box::new(geometry::Metal{albedo: colour::new(0.8, 0.8, 0.8), fuzz: 0.0});
-    let radius = 0.5;
-    scene.add(Box::new(Sphere::new(point3::new(-0.80, radius, -1.0), radius, m3)));
+    // // Shiny metal sphere
+    // let m3: Box<dyn Material> = Box::new(geometry::Metal{albedo: colour::new(0.8, 0.8, 0.8), fuzz: 0.0});
+    // let radius = 0.5;
+    // scene.add(Box::new(Sphere::new(point3::new(-0.80, radius, -1.0), radius, m3)));
 
-    // Solid glass sphere
-    let m4: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(1.0,1.0,1.0), index_of_refraction: 1.5});
-    let radius = 0.1;
-    scene.add(Box::new(Sphere::new(point3::new(0.25, 0.75, -0.5), radius, m4)));
+    // // Solid glass sphere
+    // let m4: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(1.0,1.0,1.0), index_of_refraction: 1.5});
+    // let radius = 0.1;
+    // scene.add(Box::new(Sphere::new(point3::new(0.25, 0.75, -0.5), radius, m4)));
 
-    // Hollow glass sphere
-    let m5: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(0.95,0.95,1.0), index_of_refraction: 1.5});
-    scene.add(Box::new(Sphere::new(point3::new(-0.25, 0.75, -0.42), 0.14, m5)));
-    let m5: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(0.95,0.95,1.0), index_of_refraction: 1.5});
-    scene.add(Box::new(Sphere::new(point3::new(-0.25, 0.75, -0.42), -0.13, m5)));
+    // // Hollow glass sphere
+    // let m5: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(0.95,0.95,1.0), index_of_refraction: 1.5});
+    // scene.add(Box::new(Sphere::new(point3::new(-0.25, 0.75, -0.42), 0.14, m5)));
+    // let m5: Box<dyn Material> = Box::new(geometry::Dielectric{albedo: colour::new(0.95,0.95,1.0), index_of_refraction: 1.5});
+    // scene.add(Box::new(Sphere::new(point3::new(-0.25, 0.75, -0.42), -0.13, m5)));
 
-    // Cube!
-    let m6: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.7, 0.3, 0.7)});
-    let w = 0.50; let h = 0.50; let d = 0.50;
-    scene.add(Box::new(Cube::new(point3::new(0.0, 0.5, -1.0), w,h,d, m6)));
+    // // Cube!
+    // let m6: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.7, 0.3, 0.7)});
+    // let w = 0.50; let h = 0.50; let d = 0.50;
+    // scene.add(Box::new(Cube::new(point3::new(0.0, 0.5, -1.0), w,h,d, m6)));
 
-    // Plane
-    let m6: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.3, 0.3, 0.3)});
-    let single_sided = true;
-    scene.add(Box::new(Plane::new(point3::new(0.0,1.0,0.0), point3::new(0.0,0.0,0.0), m6, single_sided)));
+    // // Plane
+    // let m6: Box<dyn Material> = Box::new(geometry::Lambertian{albedo: colour::new(0.3, 0.3, 0.3)});
+    // let single_sided = true;
+    // scene.add(Box::new(Plane::new(point3::new(0.0,1.0,0.0), point3::new(0.0,0.0,0.0), m6, single_sided)));
 
     if !USE_BUFFER{ print!("P3\n{} {}\n255\n", image_width, image_height);}
 
@@ -238,7 +239,7 @@ fn main(){
         }
         
 
-        if ctr%(image_width*2)==0{
+        if ctr%(image_width*25)==0{
             write_to_window(&mut window, &mut img_buffer, image_width, image_height);
         }
         if ctr == total_num_pxls{
@@ -307,7 +308,7 @@ struct PxData{
 
 fn calculate_some_pxls(thread_id: usize,
     num_threads: usize,
-    scene: &HittableList,
+    _scene: &HittableList,
     cam: &Camera,
     sender: &crossbeam::Sender<PxData>,
     image_height: usize,
@@ -318,6 +319,7 @@ fn calculate_some_pxls(thread_id: usize,
     blue_noise_disc: Vec<(f64,f64)>){
     
     for j in (thread_id .. image_height).step_by(num_threads){
+        let scene = HittableList::get_scene();
         // Debug msg
         // eprint!("\rScanlines remaining: {}     ", j);
         for i in 0..image_width{
@@ -338,7 +340,7 @@ fn calculate_some_pxls(thread_id: usize,
                 // cleanup_for_dof.z = 0.0;
                 let r = cam.get_ray(u, v, for_depth_of_field);
                 // let a = BlueNoise::get_screenspace((i, j, image_width));
-                px_colour += ray_colour(&r, scene, max_ray_bounces, gamma_correction, (i,j,image_width));
+                px_colour += ray_colour(&r, &scene, max_ray_bounces, gamma_correction, (i,j,image_width));
             }
             let row;
             if USE_BUFFER{ row = image_height-1-j; }else{ row = j;}
